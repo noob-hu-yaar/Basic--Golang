5c79e03ccbf91af920ca2edacc801d71732809da
noob-hu-yaar <48630662+noob-hu-yaar@users.noreply.github.com>
2020-05-11 16:09:32 +0530

    Add files via upload

---
+++ ind.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"text/template"
+	"log"
+	"os"
+)
+
+type Person struct {
+	Name string
+	Hobbies []string
+}
+
+const data = `{{$name := .Name}}
+{{range.Hobbies}} {{$name}} likes to {{.}}.
+{{end}}`
+
+func main() {
+
+	person := Person{
+		Name: "Baibhab",
+		Hobbies: []string{"Play","Code","Sing"},
+	}
+
+	template1 := template.New("test")
+	template1,err := template1.Parse(data)
+	if err != nil{
+		log.Fatal(err)
+		return
+	}
+
+	template1.Execute(os.Stdout, person)
+}
